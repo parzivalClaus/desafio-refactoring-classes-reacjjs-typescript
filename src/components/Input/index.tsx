fa07b88395743d0b33085b6f02cac2724dca5a09
claudio.goncalves <claudio.goncalves@mjv.com.br>
2021-09-28 16:39:25 -0300

    finished

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,19 +1,23 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import { useEffect, useRef, useState, useCallback } from "react";
 
-import { useField } from '@unform/core';
+import { useField } from "@unform/core";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface IInput {
+  name: any;
+  icon?: any;
+}
+
+const Input = ({
+  name,
+  icon: Icon,
+  ...rest
+}: IInput & React.InputHTMLAttributes<HTMLInputElement>) => {
+  const inputRef = useRef<any>(null);
 
   const [isFocused, setIsFocused] = useState(false);
-  const [isFilled, setIsFilled] = useState(false);
+  const [isFilled, setIsFilled] = useState<any>(false);
 
   const { fieldName, defaultValue, registerField } = useField(name);
 
@@ -31,7 +35,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
